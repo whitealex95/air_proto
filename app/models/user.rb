fd5aee7f9b060639cedba0ebd874194e46a860f0
whitealex95 <whitealex95@naver.com>
2017-08-10 16:13:43 +0900

    login fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :password, confirmation: true, length: { minimum: 7 }
+  validates :password, confirmation: true
   validates :password_confirmation, presence: true
   has_secure_password
 end
